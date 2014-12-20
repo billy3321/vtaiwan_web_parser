36b26abe0055e207369c375fbdd230be4740d936
billy3321 <billy3321@gmail.com>
2014-12-20 21:16:28 +0800

    fix bugs, comment user now works

--- app/models/article.rb
+++ app/models/article.rb
@@ -7,7 +7,7 @@ class Article < ActiveRecord::Base
     source_uri = URI.parse(self.source_url)
     if ['www.facebook.com'].include?(source_uri.try(:host))
       agent = Mechanize.new
-      reult = agent.get(self.source_url)
+      result = agent.get(self.source_url)
       parse_facebook_content(result.body)
     elsif ['www.ptt.cc'].include?(source_uri.try(:host))
       if source_uri.path.include?('Gossiping')
@@ -30,31 +30,36 @@ class Article < ActiveRecord::Base
     date_string = info_section.css('div.article-metaline span.article-meta-value')[2].text
     self.date = Time.parse(date_string)
     info_section.search('.//div').remove
-    self.content = content.gsub("\n", '<br />')
+    self.content = info_section.text.gsub("\n", '<br />')
     old_comment_author = ""
     puts self.comment_authors
-    if self.comment_authors
-      comment_authors = self.comment_authors.split(',')
-      comment_authors.collect(&:strip)
-      puts comment_authors
+    unless self.comment_authors.strip.empty?
+      @comment_authors_list = self.comment_authors.split(',')
+      @comment_authors_list.collect(&:strip)
     else
-      comment_authors = []
+      @comment_authors_list = []
     end
     comment = nil
+    self.comments.delete_all
     pushes.each do |p|
       puts p.text
       comment_author = p.css('span.push-userid')[0].text
-      if comment_authors.empty? or comment_authors.include?(comment_author)
-        comment = self.comments.build
-        comment.author = comment_author
-        comment.content = p.css('span.push-content')[0].text
+      if @comment_authors_list.empty? or @comment_authors_list.include?(comment_author)
+        if comment and comment.author == comment_author
+          comment.content << "<br />" + p.css('span.push-content')[0].text
+        else
+          comment = self.comments.build
+          comment.author = comment_author
+          comment.content = p.css('span.push-content')[0].text
+        end
       end
     end
   end
 
 
-  def self.parse_facebook_content
+  def parse_facebook_content(body)
     html = Nokogiri::HTML(body)
+    return html
     self.title = html.title
     content = html.css('div.userContent')[0].text
     self.content = content.gsub("\n", '<br />')
